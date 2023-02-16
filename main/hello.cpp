6926b5ebafebcf00139d7a1e3a46e35e257e285b
Shaarvari Kiran <71651853+shaar-cloud@users.noreply.github.com>
2023-02-16 12:00:48 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, SRN: PES1UG20CS389, have successfully built and run\n";
+  return 0;
+}
