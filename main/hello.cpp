783eacd417fafe64810e0e521b3788f98736f9db
Shaarvari Kiran <71651853+shaar-cloud@users.noreply.github.com>
2023-02-16 12:32:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I, SRN: PES1UG20CS389, have successfully built and run\n";
+  cout << "I, Shaarvari Kiram, SRN: PES1UG20CS389, have successfully built and run\n";
   return 0;
 }
